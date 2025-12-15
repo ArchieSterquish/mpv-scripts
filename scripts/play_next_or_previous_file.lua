9c3f56c6201ce081f0a2923d168301ed24858d7c
ArchieSterquish <nompos55@gmail.com>
2025-12-15 22:36:27 +0300

    Removed comments and temp file

--- scripts/play_next_or_previous_file.lua
+++ scripts/play_next_or_previous_file.lua
@@ -1,19 +1,4 @@
-﻿-- This script will force open next or previous file in the currently playing files directory.
--- Settings
--- 
--- Set them inside the settings variable at the head of the lua file. Should work out of the box.
--- 
---     filtering by filetype
---     Allow/disallow looping
---     linux/windows
--- 
--- keybindings
--- 
--- You can copy paste below into your input.conf if you want to change the keybindings.
--- shift+LEFT script-binding previousfile
--- shift+RIGHT script-binding nextfile
-
-local utils = require 'mp.utils'
+﻿local utils = require 'mp.utils'
 local msg = require 'mp.msg'
 local settings = {
   filetypes = {
--- scripts/remember_bluetooth_latency.lua
+++ scripts/remember_bluetooth_latency.lua
@@ -1,19 +1,4 @@
-﻿-- Auto mpv A-V delay on bluetooth device
--- 
--- Lua script which adds desired a-v delay if default audio sink is a bluetooth device.
--- 
---     Supports: Linux
--- 
--- Setup
--- 
--- Copy bluetooth-auto-audio-latency.lua into ~/.config/mpv/scripts/
--- Customization
--- 
--- Change A-V delay in bluetooth-auto-audio-latency.lua. By default it is -200ms.
--- 
--- latency = -0.2 -- change here according to desired latency, in seconds
-
-require 'io'
+﻿require 'io'
 require 'mp'
 latency = -0.2 -- change here according to desired latency, in seconds
 local utils = require("mp.utils")
